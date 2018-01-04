8e4db23505eb47fb6be5abfdea84b229f6365d96
pbca26 <pbca26@gmail.com>
2018-01-04 19:09:48 +0300

    coins list update

--- mmstart.sh
+++ mmstart.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 source passphrase
-source coins
-pkill -15 marketmaker;
-cd ..;
+source mmcoinsexport
+pkill -15 marketmaker
+cd ..
 ./marketmaker "{\"gui\":\"nogui\",\"client\":1, \"userhome\":\"/${HOME#"/"}\", \"passphrase\":\"$passphrase\", \"coins\":$coins}" &
-sleep 3;
+sleep 3
 curl --url "http://127.0.0.1:7783" --data "{\"userpass\":\"470f8d83cf4389502d7cf20de971e61cbeb836365e8daca4df0131fa7e374a60\",\"method\":\"getcoins\"}"
+curl "http://46.20.235.46:8111/api/mm/coins/start"
+sleep 10
+curl "http://46.20.235.46:8111/api/mm/prices/start"
+sleep 5
+curl "http://46.20.235.46:8111/api/mm/orderbook/start"
