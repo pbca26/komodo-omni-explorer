cf742b5c7d340b5f2b04db27ff8a9d23eaaaadf2
Miika Turunen <miika.turunen@gmail.com>
2018-01-25 00:17:37 +0200

    Remove unnecessary html from main.js, remove inline styles & small css fixes

--- ui/src/components/main/main.js
+++ ui/src/components/main/main.js
@@ -88,128 +88,99 @@ class Main extends React.Component {
       <div>
         <Navigation />
         <div className="container-fluid">
+          <div className="row text-center">
+            <div className="col-md-2 col-md-offset-3">
+              <div className="panel panel-default hidden-sm hidden-xs">
+                <div className="panel-heading">
+                  <strong>Coins</strong>
+                </div>
+                <div className="panel-body">
+                  <label id="hashrate">17</label>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-2 col-sm-12">
+              <img
+                src={ `http://${config.ip}:${config.port}/public/images/kmd-logo.png` }
+                alt="Komodo logo"
+                height="100px" />
+            </div>
+            { this.props.Main.fiatRates &&
+              <div className="col-md-2">
+                <div className="panel panel-default hidden-sm hidden-xs">
+                  <div className="panel-heading">
+                    <strong>KMD Price</strong>
+                  </div>
+                  <div className="panel-body">
+                    <div>
+                      <label id="lastPrice">{ this.props.Main.fiatRates.BTC } BTC</label>
+                    </div>
+                    <div>
+                      <label id="lastPrice">${ this.props.Main.fiatRates.USD }</label>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            }
+            </div>
+            { this.props.path !== '/coins' &&
+              <div className="row text-center margin-top-md margin-bottom-xlg">
+                { this.renderCoinIcons() }
+              </div>
+            }
+            { this.props.path == '/interest' ||
+              this.props.path == '/' &&
+              <div className="row text-center margin-top-md margin-bottom-xlg">
+                <div className="form-inline">
+                  <div
+                    id="index-search"
+                    className="form-group">
+                    <input
+                      onChange={ (event) => this.updateInput(event) }
+                      type="text"
+                      name="searchTerm"
+                      value={ this.state.searchTerm }
+                      placeholder={ this.props.path === '/interest' ? 'Enter a valid KMD address' : 'You may enter a tx hash or an address.' }
+                      className="form-control" />
+                    <button
+                      onClick={ this.triggerSearch }
+                      disabled={ this.state.searchTerm.length < 34 }
+                      type="submit"
+                      className="btn btn-success">Search</button>
+                  </div>
+                </div>
+              </div>
+            }
+          { this.props.children }
+        </div>
+        <footer className="site-footer hidden-xs">
           <div className="row">
             <div className="col-md-12">
-              <div className="row text-center">
-                <div className="col-md-2 col-md-offset-3">
-                  <div className="panel panel-default hidden-sm hidden-xs">
-                    <div className="panel-heading">
-                      <strong>Coins</strong>
-                    </div>
-                    <div className="panel-body">
-                      <label id="hashrate">17</label>
-                    </div>
-                  </div>
-                </div>
-                <div className="col-md-2 col-sm-12">
-                  <div>
-                    <img
-                      src={ `http://${config.ip}:${config.port}/public/images/kmd-logo.png` }
-                      alt="Komodo logo"
-                      height="100px" />
-                  </div>
-                </div>
-                { this.props.Main.fiatRates &&
-                  <div className="col-md-2">
-                    <div className="panel panel-default hidden-sm hidden-xs">
-                      <div className="panel-heading">
-                        <strong>KMD Price</strong>
-                      </div>
-                      <div className="panel-body">
-                        <div>
-                          <label id="lastPrice">{ this.props.Main.fiatRates.BTC } BTC</label>
-                        </div>
-                        <div>
-                          <label id="lastPrice">${ this.props.Main.fiatRates.USD }</label>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                }
-              </div>
-              { this.props.path !== '/coins' &&
-                <div
-                  className="row text-center"
-                  style={{ marginTop: '10px', marginBottom: '40px' }}>
-                  { this.renderCoinIcons() }
-                </div>
-              }
-              { this.props.path !== '/summary' &&
-                this.props.path !== '/interest-calc' &&
-                this.props.path !== '/prices' &&
-                !this.props.path.startsWith('/books') &&
-                this.props.path !== '/calc' &&
-                this.props.path !== '/coins' &&
-                !this.props.path.startsWith('/charts') &&
-                <div
-                  style={{ marginTop: '10px', marginBottom: '40px' }}
-                  className="row text-center">
-                  <div className="form-inline">
-                    <div
-                      id="index-search"
-                      className="form-group">
-                      <input
-                        onChange={ (event) => this.updateInput(event) }
-                        type="text"
-                        name="searchTerm"
-                        value={ this.state.searchTerm }
-                        placeholder={ this.props.path === '/interest' ? 'Enter a valid KMD address' : 'You may enter a tx hash or an address.' }
-                        style={{ minWidth: '80%', marginRight: '5px' }}
-                        className="form-control" />
-                      <button
-                        onClick={ this.triggerSearch }
-                        disabled={ this.state.searchTerm.length < 34 }
-                        type="submit"
-                        className="btn btn-success">Search</button>
-                    </div>
-                  </div>
-                </div>
-              }
+              <a
+                id="twitter-icon"
+                href="https://twitter.com/KomodoPlatform"
+                target="_blank">
+                <span className="glyphicon fa fa-twitter"></span>
+              </a>
+              <p className="margin-top-md text-center">
+                Powered by<br />
+                <a
+                  href="https://github.com/iquidus/explorer"
+                  target="_blank"
+                  className="navbar-link">Iquidus Explorer</a>,&nbsp;
+                <a
+                  href="hhttps://github.com/jl777/SuperNET"
+                  target="_blank"
+                  className="navbar-link">BarterDEX</a>
+                <span>&nbsp;&amp;&nbsp;</span>
+                <a
+                  href="https://github.com/kyuupichan/electrumx"
+                  target="_blank"
+                  className="navbar-link">Electrum</a>
+              </p>
             </div>
           </div>
-
-          { this.props.children }
-    
-        </div>
-
-        <div className="navbar navbar-default navbar-fixed-bottom hidden-xs">
-          <div className="col-md-4">
-            <ul className="nav navbar-nav">
-              <li className="pull-left">
-                <a
-                  id="twitter-icon"
-                  href="https://twitter.com/KomodoPlatform"
-                  target="_blank">
-                  <span className="glyphicon fa fa-twitter"></span>
-                </a>
-              </li>
-            </ul>
-           </div>
-           <div className="col-md-5">
-            <ul className="nav">
-              <li
-                style={{ marginLeft: '80px', marginRight: '80px' }}
-                className="text-center">
-                <p style={{ marginTop: '15px' }}>
-                  Powered by&nbsp;
-                  <a
-                    href="https://github.com/iquidus/explorer"
-                    target="_blank"
-                    className="navbar-link">Iquidus Explorer</a>,&nbsp;
-                  <a
-                    href="hhttps://github.com/jl777/SuperNET"
-                    target="_blank"
-                    className="navbar-link">BarterDEX</a>,
-                  <span style={{ color: '#fff', marginLeft: '5px', marginRight: '5px' }}>&amp;</span>
-                  <a
-                    href="https://github.com/kyuupichan/electrumx"
-                    target="_blank"
-                    className="navbar-link">Electrum</a>
-                </p>
-              </li>
-            </ul>
-           </div>
-        </div>
+        </footer>
       </div>
     );
   }
