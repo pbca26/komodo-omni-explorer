5d980b769d190c4a73b05a53d0abedf54767334d
pbca26 <pbca26@gmail.com>
2018-06-28 22:44:28 +0300

    ui update explorers list

--- ui/src/components/main/summary.js
+++ ui/src/components/main/summary.js
@@ -8,15 +8,31 @@ const explorers = {
   'KMD': [
     'http://kmd.explorer.supernet.org',
     'http://www.kmdexplorer.ru',
+    'https://kmdexplorer.io',
     'https://www.kmd.host',
     'https://explorer.komodo.services',
+    'http://kmd.explorer.dexstats.info'
   ],
-  'CHAIN': 'http://explorer.chainmakers.co',
-  'OOT': 'https://ootexplorer.com',
-  'ETOMIC': 'http://etomic.kmd.host',
+  'CHAIN': [
+    'http://explorer.chainmakers.co',
+    'https://chain.kmdexplorer.io',
+    'http://chain.explorer.dexstats.info'
+  ],
+  'OOT': [
+    'https://ootexplorer.com',
+    'https://oot.kmdexplorer.io',
+    'http://oot.explorer.dexstats.info'
+  ],
+  'ETOMIC': [
+    'https://etomic.kmdexplorer.io',
+    'http://etomic.kmd.host',
+    'http://etomic.explorer.dexstats.info'
+  ],
   'BEER': [
+    'https://beer.kmdexplorer.io',
     'http://beer.komodochainz.info',
-    'http://beet.kmd.host'
+    'http://beet.kmd.host',
+    'http://beer.explorer.dexstats.info'
   ],
   'CHIPS': [
     'http://chips1.explorer.supernet.org',
@@ -24,46 +40,143 @@ const explorers = {
     'http://chips3.explorer.supernet.org'
   ],
   'BTCH': [
+    'https://btch.kmdexplorer.io',
     'http://www.btch.host',
-    'http://btch.kmd.host'
+    'http://btch.kmd.host',
+    'http://btch.explorer.dexstats.info'
   ],
   'COQUI': [
+    'https://coqui.kmdexplorer.io',
     'https://coqui.kmdexplorer.ru',
-    'https://explorer.coqui.cash'
+    'https://explorer.coqui.cash',
+    'http://coqui.explorer.dexstats.info'
   ],
-  'BET': 'http://bet.explorer.supernet.org',
-  'BOTS': 'http://bots.explorer.supernet.org',
-  'CEAL': 'http://ceal.explorer.supernet.org',
-  'CRYPTO': 'http://crypto.explorer.supernet.org',
+  'BET': [
+    'https://bet.kmdexplorer.io',
+    'http://bet.explorer.supernet.org',
+    'http://bet.explorer.dexstats.info'
+  ],
+  'BOTS': [
+    'https://bot.kmdexplorer.io',
+    'http://bots.explorer.supernet.org',
+    'http://bots.explorer.dexstats.info'
+  ],
+  'CEAL': [
+    'https://ceal.kmdexplorer.io',
+    'http://ceal.explorer.supernet.org',
+    'http://ceal.explorer.dexstats.info'
+  ],
+  'CRYPTO': [
+    'https://crypto.kmdexplorer.io',
+    'http://crypto.explorer.supernet.org',
+    'http://crypto.explorer.dexstats.info'
+  ],
   'DEX': [
+    'https://dex.kmdexplorer.io',
     'http://dex.explorer.supernet.org',
-    'http://dex.explorer.komodo.services'
+    'http://dex.explorer.komodo.services',
+    'http://dex.explorer.dexstats.info'
   ],
-  'HODL': 'http://hodl.explorer.supernet.org',
-  'JUMBLR': 'http://jumblr.explorer.supernet.org',
-  'KV': 'http://kv.explorer.supernet.org',
-  'MESH': 'http://mesh.explorer.supernet.org',
+  'HODL': [
+    'https://hodl.kmdexplorer.io',
+    'http://hodl.explorer.supernet.org',
+    'http://hodl.explorer.dexstats.info'
+  ],
+  'JUMBLR': [
+    'https://jumblr.kmdexplorer.io',
+    'http://jumblr.explorer.supernet.org',
+    'http://jumblr.explorer.dexstats.info'
+  ],
+  'KV': [
+    'https://kv.kmdexplorer.io',
+    'http://kv.explorer.supernet.org',
+    'http://kv.explorer.dexstats.info'
+  ],
+  'MESH': [
+    'https://mesh.kmdexplorer.io',
+    'http://mesh.explorer.supernet.org',
+    'http://mesh.explorer.dexstats.info'
+  ],
   'MGW': [
+    'https://mgw.kmdexplorer.io',
     'http://mgw.explorer.supernet.org',
-    'https://mgw.kmdexplorer.ru'
+    'https://mgw.kmdexplorer.ru',
+    'http://mgw.explorer.dexstats.info'
   ],
   'MSHARK': [
+    'https://mshark.kmdexplorer.io',
     'http://mshark.explorer.supernet.org',
-    'https://mshark.kmdexplorer.ru'
+    'https://mshark.kmdexplorer.ru',
+    'http://mshark.explorer.dexstats.info'
   ],
-  'MVP': 'http://mvp.explorer.supernet.org',
-  'PANGEA': 'http://pangea.explorer.supernet.org',
-  'PIZZA': 'http://pizza.komodochainz.info',
-  'REVS': 'http://revs.explorer.supernet.org',
+  'MVP': [
+    'https://mvp.kmdexplorer.io',
+    'http://mvp.explorer.supernet.org',
+  ],
+  'PANGEA': [
+    'https://pangea.kmdexplorer.io',
+    'http://pangea.explorer.supernet.org',
+    'http://pangea.explorer.dexstats.info'
+  ],
+  'PIZZA': [
+    'https://pizza.kmdexplorer.io',
+    'http://pizza.komodochainz.info',
+    'http://pizza.explorer.dexstats.info'
+  ],
+  'REVS': [
+    'https://revs.kmdexplorer.io',
+    'http://revs.explorer.supernet.org',
+    'http://revs.explorer.dexstats.info'
+  ],
   'SUPERNET': [
+    'https://supernet.kmdexplorer.io',
     'http://supernet.explorer.supernet.org',
-    'http://supernet.explorer.komodo.services'
+    'http://supernet.explorer.komodo.services',
+    'http://supernet.explorer.dexstats.info'
   ],
   'SHARK': [
+    'https://shark.kmdexplorer.io',
     'http://shark.explorer.supernet.org',
     'https://shark.kmdexplorer.ru'
   ],
-  'WLC': 'http://wireless.explorer.supernet.org',
+  'WLC': [
+    'https://wlc.kmdexplorer.io',
+    'http://wireless.explorer.supernet.org',
+    'http://wlc.explorer.dexstats.info'
+  ],
+  'AXO': [
+    'https://axo.kmdexplorer.io',
+    'http://axo.explorer.dexstats.info'
+  ],
+  'DSEC': [
+    'http://dsec.explorer.dexstats.info',
+    'https://dsec.kmdexplorer.io'
+  ],
+  /*'NINJA': [
+    'http://ninja.explorer.dexstats.info'
+    'https://ninja.kmdexplorer.io'
+  ],*/
+  'GLXT': [
+    'http://glxt.explorer.dexstats.info',
+    'https://glxt.kmdexplorer.io'
+  ],
+  'PRLPAY': [
+    'http://prlpay.explorer.dexstats.info',
+    'https://prlpay.kmdexplorer.io'
+  ],
+  'BNTN': [
+    'http://bntn.explorer.dexstats.info',
+    'https://bntn.kmdexplorer.io',
+  ],
+  'EQL': [
+    'http://eql.explorer.dexstats.info',
+    'https://eql.kmdexplorer.io'
+  ],
+  'VRSC': [
+    'https://explorer.veruscoin.io',
+    'https://vrsc.kmdexplorer.io'
+  ],
+  'ZILLA': 'http://zilla.explorer.dexstats.info',
 };
 
 class Summary extends React.Component {
