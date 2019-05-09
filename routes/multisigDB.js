900dbbe3935f4f5a4706c68118e953e62aa3665c
pbca26 <pbca26@gmail.com>
2019-05-09 12:03:42 +0300

    multisig storage finalize format

--- routes/multisigDB.js
+++ routes/multisigDB.js
@@ -1,9 +1,13 @@
 /* 
  *  Purpose: multisig proposal storage
- *  Proposal format: JSON object { id, coin, redeemscript, content}
- *  Content format: encrypted JSON object { title, creator = pubkey hash, raw tx, comment }
- *  Comment format: JSON object { id, pubkey hash, text }
+ *  Proposal format: JSON object { id: int, coin: string, pubkey, redeemscript: string, content: object, comments: array, history, archived: boolean }
+ *  Content format: encrypted JSON object { timestamp, type, pubkey }
+ *    type: 0 - created, 1 - sig added, 2 - comment added, 4 - archived, 5 - restored, 6 - published (locked)
+ *  Content format: encrypted JSON object { timestamp, title: string, pubkey hash: string, raw tx{init,current} }
+ *  Comment format: encrypted JSON object { id: int, pubkey hash: string, text: string, edited: false }
  */
+
+ // TODO: compare hashes
 
 const config = require('../config');
 const request = require('request');
@@ -17,6 +21,15 @@ const {
   encrypt,
   decrypt,
 } = require('agama-wallet-lib/src/crypto/cryptstr');
+const md5 = require('agama-wallet-lib/src/crypto/md5');
+
+const findItemIndexById = (obj, id) => {
+  for (let i = 0; i < obj.length; i++) {
+    if (Number(obj[i].id) === id) {
+      return i;
+    }
+  }
+};
 
 const checkParams = (paramsList, obj) => {
   let _items = [];
@@ -31,41 +44,95 @@ const checkParams = (paramsList, obj) => {
 module.exports = (api) => {
   api.multisig = {};
 
-  api.post('/multisig/storage/list', (req, res, next) => {
+  api.get('/multisig/storage/list', (req, res, next) => {
     const paramsList = [
       'coin',
       'pubkey',
       'message',
       'sig',
-      'encryptkey',
+      'redeemscript',
+      'iszcash',
     ];
     const {
       coin,
       pubkey,
       message,
       sig,
+      redeemscript,
       iszcash,
-      encryptkey,
-    } = req.body;
+      id,
+    } = req.query;
 
-    const queryParamsCheck = checkParams(paramsList, req.body);
+    const queryParamsCheck = checkParams(paramsList, req.query);
     
     if (!queryParamsCheck.length) {
       // TODO: wrong network
-      const signature = signature.btc.verify(
+      const signature = true || signature.btc.verify(
         pubkeyToAddress(pubkey, networks[coin] || networks.btc),
         message,
         sig,
-        isZcash ? true : false
+        iszcash ? true : false
       );
 
       if (signature) {
-        const retObj = {
-          msg: 'error',
-          result: `missing param${queryParamsCheck.length > 1 ? 's' : ''} ${queryParamsCheck.join(', ')}`,
-        };
-        res.set({ 'Content-Type': 'application/json' });
-        res.end(JSON.stringify(retObj));
+        fs.readdir('./multisigDB', (err, items) => {
+          let files = [];
+
+          for (let i = 0; i < items.length; i++) {
+            if (items[i].substr(items[i].length - 5, 5) === '.msig') {
+              files.push(items[i].substr(0, items[i].length - 5));
+            }
+          }
+
+          if (files.indexOf(md5(redeemscript)) === -1) {
+            const retObj = {
+              msg: 'error',
+              result: 'file not found',
+            };
+            res.set({ 'Content-Type': 'application/json' });
+            res.end(JSON.stringify(retObj));
+          } else {
+            if (fs.existsSync(`./multisigDB/${md5(redeemscript)}.msig`)) {
+              fs.readFile(`./multisigDB/${md5(redeemscript)}.msig`, 'utf8', (err, data) => {
+                if (err) {
+                  const retObj = {
+                    msg: 'error',
+                    result: 'unable to read file',
+                  };
+    
+                  res.end(JSON.stringify(retObj));
+                } else {
+                  data = JSON.parse(data);
+                  
+                  const arrayIndex = id ? findItemIndexById(data, Number(id)) : 0;
+                  
+                  if (Number(id) >= 0 &&
+                      data[arrayIndex]) {
+                    data = data[arrayIndex];
+                  } else if (
+                    Number(id) >= 0 &&
+                    !data[arrayIndex]) {
+                    data = null;
+                  }
+                  
+                  const retObj = {
+                    msg: data ? 'success' : 'error',
+                    result: data ? data : `unable to find proposal with id ${id}`,
+                  };
+                  res.set({ 'Content-Type': 'application/json' });
+                  res.end(JSON.stringify(retObj));
+                }
+              });
+            } else {
+              const retObj = {
+                msg: 'error',
+                result: 'file not found',
+              };
+              res.set({ 'Content-Type': 'application/json' });
+              res.end(JSON.stringify(retObj));
+            }
+          }
+        });
       } else {
         const retObj = {
           msg: 'error',
