3ba84bd389ecc76bbc2880b687d05292c3d9c390
pbca26 <pbca26@gmail.com>
2019-08-07 19:51:35 +0300

    electrum protocol version check fix

--- routes/electrumGetServerVersion.js
+++ routes/electrumGetServerVersion.js
@@ -1,21 +1,20 @@
+// TODO: caching
+
 module.exports = (api) => {
   api.getServerVersion = (ecl) => {
-    //const ecl = new electrumJSCore(port, ip, proto);
     let protocolVersion;
     
     return new Promise((resolve, reject) => {
-      //ecl.connect();
       ecl.serverVersion()
       .then((serverData) => {
-        //ecl.close();
 
         let serverVersion = 0;
 
         if (serverData &&
-          typeof serverData === 'object' &&
-          serverData[0] &&
-          serverData[0].indexOf('ElectrumX') > -1 &&
-          Number(serverData[1])
+            typeof serverData === 'object' &&
+            serverData[0] &&
+            serverData[0].indexOf('ElectrumX') > -1 &&
+            Number(serverData[1])
         ) {
           serverVersion = Number(serverData[1]);
 
@@ -23,6 +22,11 @@ module.exports = (api) => {
             protocolVersion = Number(serverData[1]);
             ecl.setProtocolVersion(protocolVersion);
           }
+        }
+
+        if (serverData.hasOwnProperty('code') &&
+            serverData.code === '-777') {
+          resolve(-777);
         }
 
         api.log(`server ${`${ecl.host}:${ecl.port}:${ecl.proto}`} protocol version: ${protocolVersion}`);
