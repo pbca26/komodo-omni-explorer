9c42210b1f5d4f9151e749f841848f6bb16bf665
pbca26 <pbca26@gmail.com>
2019-09-02 19:43:57 +0300

    multisig proposals api (incomplete)

--- routes/multisigDB.js
+++ routes/multisigDB.js
@@ -1,9 +1,9 @@
 /* 
  *  Purpose: multisig proposal storage
  *  Proposal format: JSON object { id: int, coin: string, pubkey, redeemscript: string, content: object, contentBackup: initial content copy, comments: array, history, archived: boolean }
- *  Content format: encrypted JSON object { timestamp, type, pubkey }
+ *  History format: encrypted JSON object { timestamp, type, pubkey }
  *    type: 0 - created, 1 - sig added, 2 - comment added, 4 - archived, 5 - restored, 6 - published (locked)
- *  Content format: encrypted JSON object { timestamp, title: string, pubkey hash: string, raw tx }
+ *  Content format: encrypted JSON object { timestamp, title: string, pubkey hash: string, raw tx, txid }
  *  Comment format: encrypted JSON object { id: int, pubkey hash: string, text: string, edited: false }
  */
 
@@ -22,11 +22,29 @@ const {
   decrypt,
 } = require('agama-wallet-lib/src/crypto/cryptstr');
 const md5 = require('agama-wallet-lib/src/crypto/md5');
+const { randomBytes } = require('crypto');
 
 const findItemIndexById = (obj, id) => {
   for (let i = 0; i < obj.length; i++) {
-    if (Number(obj[i].id) === id) {
+    if (obj[i].id == id) {
       return i;
+    }
+  }
+};
+
+const isLocked = (obj) => {
+  for (let i = 0; i < obj.length; i++) {
+    if (Number(obj[i].type) === 6) {
+      return true;
+    }
+  }
+};
+
+const isSigned = (obj, pubkey) => {
+  for (let i = 0; i < obj.length; i++) {
+    if (obj[i].pubkey === pubkey &&
+        Number(obj[i].type) === 1) {
+      return true;
     }
   }
 };
@@ -43,6 +61,197 @@ const checkParams = (paramsList, obj) => {
 
 module.exports = (api) => {
   api.multisig = {};
+
+  api.get('/multisig/storage/update', (req, res, next) => {
+    const paramsList = [
+      'coin',
+      'pubkey',
+      'message',
+      'sig',
+      'redeemscript',
+      'iszcash',
+      'id',
+    ];
+    const {
+      coin,
+      pubkey,
+      message,
+      sig,
+      redeemscript,
+      iszcash,
+      id,
+      restore,
+      content,
+      broadcast,
+    } = req.query;
+
+    const queryParamsCheck = checkParams(paramsList, req.query);
+    
+    if (!queryParamsCheck.length) {
+      // TODO: wrong network
+      const signature = true || signature.btc.verify(
+        pubkeyToAddress(pubkey, networks[coin] || networks.btc),
+        message,
+        sig,
+        iszcash ? true : false
+      );
+
+      if (signature) {
+        fs.readdir('./multisigDB', (err, items) => {
+          let files = [];
+
+          for (let i = 0; i < items.length; i++) {
+            if (items[i].substr(items[i].length - 5, 5) === '.msig') {
+              files.push(items[i].substr(0, items[i].length - 5));
+            }
+          }
+
+          if (files.indexOf(md5(redeemscript)) === -1) {
+            const retObj = {
+              msg: 'error',
+              result: 'file not found',
+            };
+            res.set({ 'Content-Type': 'application/json' });
+            res.end(JSON.stringify(retObj));
+          } else {
+            if (fs.existsSync(`./multisigDB/${md5(redeemscript)}.msig`)) {
+              fs.readFile(`./multisigDB/${md5(redeemscript)}.msig`, 'utf8', (err, data) => {
+                if (err) {
+                  const retObj = {
+                    msg: 'error',
+                    result: 'unable to read file',
+                  };
+    
+                  res.end(JSON.stringify(retObj));
+                } else {
+                  data = JSON.parse(data);
+                  const arrayIndex = findItemIndexById(data, id);
+                  
+                  if (arrayIndex >= 0) {
+                    if (content) {
+                      if (!isLocked(data[arrayIndex].history)) {
+                        if (broadcast) {
+                          data[arrayIndex].archived = true;
+                          data[arrayIndex].content = content;
+                          data[arrayIndex].history.push({
+                            timestamp: Math.floor(Date.now() / 1000),
+                            pubkey,
+                            type: 6,
+                          });
+                          // update proposal file
+                          fs.writeFileSync(`./multisigDB/${md5(redeemscript)}.msig`, JSON.stringify(data));
+                          
+                          const retObj = {
+                            msg: 'success',
+                            result: 'proposal updated',
+                          };
+                          res.set({ 'Content-Type': 'application/json' });
+                          res.end(JSON.stringify(retObj));
+                        } else if (!isSigned(data[arrayIndex].history, pubkey)) {
+                          data[arrayIndex].content = content;
+                          data[arrayIndex].history.push({
+                            timestamp: Math.floor(Date.now() / 1000),
+                            pubkey,
+                            type: 1,
+                          });
+                          // update proposal file
+                          fs.writeFileSync(`./multisigDB/${md5(redeemscript)}.msig`, JSON.stringify(data));
+                          
+                          const retObj = {
+                            msg: 'success',
+                            result: 'proposal updated',
+                          };
+                          res.set({ 'Content-Type': 'application/json' });
+                          res.end(JSON.stringify(retObj));
+                        } else {
+                          const retObj = {
+                            msg: 'error',
+                            result: 'you already signed',
+                          };
+                          res.set({ 'Content-Type': 'application/json' });
+                          res.end(JSON.stringify(retObj));
+                        }
+                      } else {
+                        const retObj = {
+                          msg: 'error',
+                          result: 'proposal is finalized',
+                        };
+                        res.set({ 'Content-Type': 'application/json' });
+                        res.end(JSON.stringify(retObj));
+                      }
+                    } else {
+                      if (pubkey === data[arrayIndex].pubkey) {
+                        if ((!data[arrayIndex].archived && !restore) || (data[arrayIndex].archived && restore)) {
+                          data[arrayIndex].archived = !restore ? true : false;
+                          data[arrayIndex].history.push({
+                            timestamp: Math.floor(Date.now() / 1000),
+                            pubkey,
+                            type: !restore ? 4 : 5,
+                          });
+
+                          // update proposal file
+                          fs.writeFileSync(`./multisigDB/${md5(redeemscript)}.msig`, JSON.stringify(data));
+                
+                          const retObj = {
+                            msg: 'success',
+                            result: 'proposal updated',
+                          };
+                          res.set({ 'Content-Type': 'application/json' });
+                          res.end(JSON.stringify(retObj));
+                        } else {
+                          const retObj = {
+                            msg: 'error',
+                            result: `proposal already ${restore ? 'restored' : 'archived'}`,
+                          };
+                          res.set({ 'Content-Type': 'application/json' });
+                          res.end(JSON.stringify(retObj));
+                        }
+                      } else {
+                        const retObj = {
+                          msg: 'error',
+                          result: 'only creator can archive/restore proposal',
+                        };
+                        res.set({ 'Content-Type': 'application/json' });
+                        res.end(JSON.stringify(retObj));
+                      }
+                    }
+                  } else {
+                    const retObj = {
+                      msg: 'error',
+                      result: `unable to find proposal with id ${id}`,
+                    };
+                    res.set({ 'Content-Type': 'application/json' });
+                    res.end(JSON.stringify(retObj));
+                  }
+                }
+              });
+            } else {
+              const retObj = {
+                msg: 'error',
+                result: 'file not found',
+              };
+              res.set({ 'Content-Type': 'application/json' });
+              res.end(JSON.stringify(retObj));
+            }
+          }
+        });
+      } else {
+        const retObj = {
+          msg: 'error',
+          result: 'signature mismatch',
+        };
+        res.set({ 'Content-Type': 'application/json' });
+        res.end(JSON.stringify(retObj));
+      }
+    } else {
+      const retObj = {
+        msg: 'error',
+        result: `missing param${queryParamsCheck.length > 1 ? 's' : ''} ${queryParamsCheck.join(', ')}`,
+      };
+      res.set({ 'Content-Type': 'application/json' });
+      res.end(JSON.stringify(retObj));
+    }
+  });
 
   api.get('/multisig/storage/list', (req, res, next) => {
     const paramsList = [
@@ -104,13 +313,13 @@ module.exports = (api) => {
                 } else {
                   data = JSON.parse(data);
                   
-                  const arrayIndex = id ? findItemIndexById(data, Number(id)) : 0;
+                  const arrayIndex = id ? findItemIndexById(data, id) : 0;
                   
-                  if (Number(id) >= 0 &&
+                  if (id &&
                       data[arrayIndex]) {
                     data = data[arrayIndex];
                   } else if (
-                    Number(id) >= 0 &&
+                    id &&
                     !data[arrayIndex]) {
                     data = null;
                   }
@@ -194,7 +403,7 @@ module.exports = (api) => {
           }
 
           const newProposalTemplate = {
-            id: 0,
+            id: randomBytes(16).toString('hex'),
             coin,
             redeemscript,
             content,
@@ -235,13 +444,13 @@ module.exports = (api) => {
                 } else {
                   data = JSON.parse(data);
                   data.push(newProposalTemplate);
-
+                  
                   // update proposal file
                   fs.writeFileSync(`./multisigDB/${md5(redeemscript)}.msig`, JSON.stringify(data));
                   
                   const retObj = {
                     msg: 'success',
-                    result: 'proposal updated',
+                    result: 'proposal file updated',
                   };
                   res.set({ 'Content-Type': 'application/json' });
                   res.end(JSON.stringify(retObj));
